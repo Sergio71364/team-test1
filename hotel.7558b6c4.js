6b1d0a7b09099987220dedbd90912780185c31f7
Sergio71364 <58699786+Sergio71364@users.noreply.github.com>
2022-11-29 11:00:19 +0000

    Deploying to gh-pages from @ Sergio71364/team-test1@9fddb91722fd915fe30c1ae009279649f525e822 🚀

---
+++ hotel.7558b6c4.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({prevArrow:'<button type="button" class="my-slick-prev"></button>',nextArrow:'<button type="button" class="my-slick-next"></button>',infinite:!1,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0});
+//# sourceMappingURL=hotel.7558b6c4.js.map
