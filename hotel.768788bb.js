f1fecc46cbd6b1da408414e1c62244342f6c9225
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-01-10 10:43:42 +0000

    Deploying to gh-pages from @ sergio-nezhigay/team-test1@6c29ead3e89c48a0b416d198d6c00ebcc0a57ea5 🚀

--- hotel.768788bb.js
+++
@@ -1,2 +0,0 @@
-$(".one-time").slick({prevArrow:'<button type="button" class="my-slick-prev"></button>',nextArrow:'<button type="button" class="my-slick-next"></button>',infinite:!1,speed:300,slidesToShow:2,adaptiveHeight:!0,variableWidth:!0});
-//# sourceMappingURL=hotel.768788bb.js.map
